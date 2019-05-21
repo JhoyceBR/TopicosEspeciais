f6f7cdf4ceabba482fd3ad4f47b708aad15be368
Joyce Pinheiro <joyce.real01@gmail.com>
2019-05-21 10:44:02 -0300

    Add files via upload

---
+++ MeuPrimeiroSQLite/temp.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+# conecta aí parceiro
+conn = sqlite3.connect('shallownowschool.db')
+
+# define um cursor para pegar os dados
+cursor = conn.cursor()
+cursor.execute("""
+    INSERT INTO tb_estudante(nome, endereco, nascimento, matricula)
+    VALUES('maria do bairro', 'rua da paz', '2018-01-01', '201810010010');
+
+""")
+conn.commit()
+print("inserido bem top com sucesso")
+conn.close()
