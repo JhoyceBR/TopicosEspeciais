845ad07d2705aa1fc0307dd78cea30313cacfa96
Joyce Pinheiro <joyce.real01@gmail.com>
2019-06-05 12:09:33 -0300

    Add files via upload

--- EscolaServicoApp/App.py
+++ EscolaServicoApp/App.py
@@ -16,11 +16,59 @@ def getEscolas():
     pass
 
 @app.route("/escolas/<int:id>", methods=['GET'])
-def getEscola(id): 
-    pass 
+def getEscola(id):
+    pass
 
 @app.route("/escola", methods=['POST'])
 def setEscola():
+    pass
+
+@app.route("/alunos", methods=['GET'])
+def getAlunos():
+    pass
+
+@app.route("/alunos/<int:id>", methods=['GET'])
+def getAlunos(id):
+    pass
+
+@app.route("/aluno", methods=['POST'])
+def setAlunos():
+    pass
+
+@app.route("/curso", methods=['GET'])
+def getCurso():
+    pass
+
+@app.route("/curso/<int:id>", methods=['GET'])
+def getCurso(id):
+    pass
+
+@app.route("/curso", methods=['POST'])
+def setCurso():
+    pass
+
+@app.route("/turmas", methods=['GET'])
+def getTurmas():
+    pass
+
+@app.route("/turmas/<int:id>", methods=['GET'])
+def getTurmas(id):
+    pass
+
+@app.route("/turmas", methods=['POST'])
+def setTurmas():
+    pass
+
+@app.route("/disciplinas", methods="GET")
+def getDisciplinas():
+    pass
+
+@app.route("/disciplinas/<int:id>", methods=['GET'])
+def getDisciplinas(id):
+    pass
+
+@app.route("/disciplinas", methods=['POST'])
+def setDisciplinas():
     pass
 
 app.run()
