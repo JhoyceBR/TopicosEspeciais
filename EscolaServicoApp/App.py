2e987defec5a50621182ba823c20c8c20daddcc4
Joyce Pinheiro <joyce.real01@gmail.com>
2019-08-08 08:53:13 -0300

    Atualização de verificação e correção de bugs

--- EscolaServicoApp/App.py
+++ EscolaServicoApp/App.py
@@ -22,23 +22,193 @@ logger.setLevel(logging.INFO)
 schema = JsonSchema()
 schema.init_app(app)
 
+endereco_schema = {
+    'requered': ['logradouro', 'complemento', 'bairro', 'cep', 'numero'],
+    'properties': {
+        'logradouro': {'type':''},
+        'complemento': {'type': ''},
+        'bairro': {'type': ''},
+        'cep': {'type': ''},
+        'numero': {'type': ''}
+
+    }
+}
+
+escola_schema = {
+    'requered': ['nome', 'fk_id_endereco', 'fk_id_campus'],
+    'properties': {
+        'nome': {'type': 'string'},
+        'fk_id_endereco': {'type': 'integer'},
+        'fk_id_campus': {'type': 'integer'}
+    }
+
+}
+
 aluno_schema = {
-    'requered': ['nome','matricula','cpf', 'nascimento'],
+    'requered': ['nome','matricula','cpf', 'nascimento', 'fk_id_endereco', 'fk_id_curso'],
     'properties': {
         'nome': {'type': 'string'},
         'matricula': {'type': 'string'},
         'cpf': {'type': 'string'},
-        'nascimento': {'type': 'string'}
+        'nascimento': {'type': 'string'},
+        'fk_id_endereco': {'type': 'integer'},
+        'fk_id_curso': {'type': 'integer'}
 
     }
 
 }
 
+professor_schema = {
+    'requered': ['nome', 'fk_id_endereco'],
+    'properties': {
+        'nome': {'type': 'string'},
+        'fk_id_endereco': {'type': 'integer'}
+    }
+}
+
+disciplina_schema = {
+    'requered': ['nome', 'fk_id_professor'],
+    'properties': {
+        'nome': {'type': 'string'},
+        'fk_id_professor': {'type': 'integer'}
+    }
+
+}
+
+campus_schema = {
+    'requered': ['sigla', 'cidade'],
+    'properties': {
+        'sigla': {'type': 'string'},
+        'cidade': {'type': 'string'}
+    }
+
+}
+
+turno_schema = {
+    'requered': ['nome'],
+    'properties': {
+        'nome': {'type': 'string'}
+    }
+
+}
+
+curso_schema = {
+    'requered': ['nome', 'turno', 'fk_id_turno'],
+    'properties': {
+        'nome': {'type': 'string'},
+        'turno': {'type': 'string'},
+        'fk_id_turno': {'type': 'integer'}
+    }
+
+}
+
+turma_schema = {
+    'requered': ['nome', 'fk_id_curso'],
+    'properties': {
+        'nome': {'type': 'string'},
+        'fk_id_curso': {'type': 'integer'}
+    }
+
+}
+
+Nome_DB = "EscolaApp_versao2.db"
+
+@app.route("/endereco", methods=['GET'])
+def getEnderecos():
+    logger.infor("Listando Endereços.")
+    try:
+        conn = sqlite3.connect(Nome_DB)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT *
+            FROM tb_endereco;
+        """)
+
+        enderecos = []
+        for linha in cursor.fetchall():
+            endereco = {
+                "id_endereco": linha[0],
+                "logradouro": linha[1],
+                "complemento": linha[2],
+                "bairro": linha[3],
+                "cep": linha[4],
+                "numero": linha[5]
+            }
+            enderecos.append(endereco)
+        logger.infor(enderecos)
+        conn.close()
+
+    except(sqlite3.Error):
+        logger.error("Ocorreu um erro.")
+
+    return jsonify(enderecos)
+
+@app.route("/enderecos/<int:id>", methods=['GET'])
+def getEndereco(id):
+    logger.info("Listando endereços por id: %s"%(id))
+    try:
+        conn = sqlite3.connect(Nome_DB)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT *
+            FROM tb_endereco
+            WHERE id_endereco = ?;
+        """, (id, ))
+
+        linha = cursor.fechone()
+        endereco = {
+            "id_endereco": linha[0],
+            "logradouro": linha[1],
+            "complemento": linha[2],
+            "bairro": linha[3],
+            "cep": linha[4],
+            "numero": linha[5]
+        }
+
+        conn.close()
+    except sqlite3.Error:
+        return("Ocorreu um erro.")
+
+    return(jsonify(endereco))
+
+@app.route("/endereco", methods=['POST'])
+@schema.validate(endereco_schema)
+def setEndereco():
+    logger.info("Cadastrando endereco.")
+    try:
+        enderecoJson = request.get_json()
+        logradouro = enderecoJson['logradouro'],
+        complemento = enderecoJson['complemento'],
+        bairro = enderecoJson['bairro'],
+        cep = enderecoJson['cep'],
+        numero = enderecoJson['numero']
+        endereco = Endereco(logradouro,complemento, bairro, cep, numero)
+
+
+        conn = sqlite3.connect(Nome_DB)
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO tb_endereco(logradouro, complemento, bairro, cep, numero)
+            VALUES(?, ?, ?, ?, ?);
+        """, (logradouro, complemento, bairro, cep, numero))
+        conn.commit()
+        conn.close()
+
+        id = cursor.lastrowid
+        escola['id_endereco'] = id
+    except sqlite3.Error:
+        return("Ocorreu um erro.")
+
+    return jsonify(endereco)
+
+
 @app.route("/escolas", methods=['GET'])
 def getEscolas():
     logger.info("Listanto escolas.")
     try:
-        conn = sqlite3.connect('ifpb.db')
+        conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
 
         cursor.execute("""
@@ -51,10 +221,11 @@ def getEscolas():
             escola = {
                 "id_escola":linha[0],
                 "nome":linha[1],
-                "logradouro":linha[2],
-                "cidade":linha[3]
+                "id_fk_endereco": linha[2],
+                "id_fk_campus":linha[3]
             }
             escolas.append(escola)
+        logger.info(escolas)
 
         conn.close()
     except sqlite3.Error:
@@ -62,12 +233,12 @@ def getEscolas():
 
     return jsonify(escolas)
 
-#####ver aqui
+
 @app.route("/escolas/<int:id>", methods=['GET'])
 def getEscola(id):
-    logger.info("Listanto escola pelo id.")
+    logger.info("Listanto escola pelo id: %s" %(id))
     try:
-        conn = sqlite3.connect('ifpb.db')
+        conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
 
         cursor.execute("""
@@ -78,8 +249,8 @@ def getEscola(id):
         escola = {
             "id_escola":linha[0],
             "nome":linha[1],
-            "logradouro":linha[2],
-            "cidade":linha[3]
+            "id_fk_endereco":linha[2],
+            "fk_id_campus":linha[3]
         }
 
         conn.close()
@@ -90,20 +261,22 @@ def getEscola(id):
 
 
 @app.route("/escola", methods=['POST'])
+@schema.validate(escola_schema)
 def setEscola():
     logger.info("Cadastrando escola.")
     try:
-        escola = request.get_json()
-        nome = escola['nome'] # valor indicado no dicionário
-        # logradouro = escola['logradouro']
-        # cidade = escola['cidade']
+        escolaJson = request.get_json()
+        nome = escolaJson['nome']
+        id_fk_endereco = escolaJson['id_fk_endereco']
+        id_fk_campus = escolaJson['id_fk_campus']
+        escola = Escola(nome, id_fk_endereco, id_fk_campus)
 
-        conn = sqlite3.connect("ifpb.db")
+        conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT INTO tb_escola(nome, logradouro, cidade)
+            INSERT INTO tb_escola(nome, id_fk_endereco, id_fk_campus)
             VALUES(?, ?, ?);
-        """, (nome, logradouro, cidade))
+        """, (nome, id_fk_endereco, id_fk_campus))
         conn.commit()
         conn.close()
 
@@ -119,7 +292,7 @@ def setEscola():
 def getAlunos():
     logger.info("Listanto alunos.")
     try:
-        conn = sqlite3.connect('ifpb.db')
+        conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
 
         cursor.execute("""
@@ -135,10 +308,12 @@ def getAlunos():
                 "nome":linha[1],
                 "matricula":linha[2],
                 "cpf":linha[3],
-                "nascimento":linha[4]
+                "nascimento":linha[4],
+                "fk_id_endereco": linha[5],
+                "fk_id_curso": linha[6]
             }
             alunos.append(aluno)
-
+        logger.info(alunos)
         conn.close()
     except sqlite3.Error:
         return("Ocorreu um erro.")
@@ -146,9 +321,10 @@ def getAlunos():
 
 
 @app.route("/alunos/<int:id>", methods=['GET'])
+# colocar o try! 
 def getAluno(id):
-    logger.info("Listanto aluno pelo id.")
-    conn = sqlite3.connect('ifpb.db')
+    logger.info("Listanto aluno pelo id: %s."%(id))
+    conn = sqlite3.connect(Nome_DB)
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -162,7 +338,9 @@ def getAluno(id):
         "nome":linha[1],
         "matricula":linha[2],
         "cpf":linha[3],
-        "nascimento":linha[4]
+        "nascimento":linha[4],
+        "fk_id_endereco": linha[5],
+        "fk_id_curso": linha[6]
     }
 
     conn.close()
@@ -173,18 +351,19 @@ def getAluno(id):
 @schema.validate(aluno_schema)
 def setAlunos():
     logger.info("Cadastrando aluno.")
-    aluno = request.get_json() # recupera json completo - dicionario
-    nome = aluno['nome']
-    matricula = aluno['matricula']
-    cpf = aluno['cpf']
-    nascimento = aluno['nascimento']
+    alunoJson = request.get_json()
+    nome = alunoJson['nome']
+    matricula = alunoJson['matricula']
+    cpf = alunoJson['cpf']
+    nascimento = alunoJson['nascimento']
+    aluno = Aluno(nome, matricula, cpf, nascimento, fk_id_endereco, fk_id_curso)
 
-    conn = sqlite3.connect("ifpb.db")
+    conn = sqlite3.connect(Nome_DB)
     cursor = conn.cursor()
     cursor.execute("""
-        INSERT INTO tb_aluno(nome, matricula, cpf, nascimento)
-        VALUES(?, ?, ?, ?);
-    """, (nome, matricula, cpf, nascimento))
+        INSERT INTO tb_aluno(nome, matricula, cpf, nascimento, fk_id_endereco, fk_id_curso)
+        VALUES(?, ?, ?, ?, ?, ?);
+    """, (nome, matricula, cpf, nascimento, fk_id_endereco, fk_id_curso))
     conn.commit()
     conn.close()
 
@@ -196,7 +375,7 @@ def setAlunos():
 @app.route("/cursos", methods=['GET'])
 def getCursos():
     logger.info("Listanto cursos.")
-    conn = sqlite3.connect('ifpb.db')
+    conn = sqlite3.connect(Nome_DB)
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -220,7 +399,7 @@ def getCursos():
 @app.route("/cursos/<int:id>", methods=['GET'])
 def getCurso(id):
     logger.info("Listanto curso pelo id.")
-    conn = sqlite3.connect('ifpb.db')
+    conn = sqlite3.connect(Nome_DB)
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -246,7 +425,7 @@ def setCurso():
         nome = curso['nome']
         turno = curso['turno']
 
-        conn = sqlite3.connect("ifpb.db")
+        conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
         cursor.execute("""
             INSERT INTO tb_curso(nome, turno)
@@ -266,7 +445,7 @@ def setCurso():
 @app.route("/turmas", methods=['GET'])
 def getTurmas():
     logger.info("Listanto turmas.")
-    conn = sqlite3.connect('ifpb.db')
+    conn = sqlite3.connect(Nome_DB)
 
     cursor = conn.cursor()
 
@@ -291,7 +470,7 @@ def getTurmas():
 @app.route("/turmas/<int:id>", methods=['GET'])
 def getTurma(id):
     logger.info("Listanto turma pelo id.")
-    conn = sqlite3.connect('ifpb.db')
+    conn = sqlite3.connect(Nome_DB)
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -317,7 +496,7 @@ def setTurmas():
     nome = turma['nome']
     curso = turma['curso']
 
-    conn = sqlite3.connect("ifpb.db")
+    conn = sqlite3.connect(Nome_DB)
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO tb_turma(nome, curso)
@@ -335,7 +514,7 @@ def setTurmas():
 @app.route("/disciplinas", methods=["GET"])
 def getDisciplinas():
     logger.info("Listanto disciplinas.")
-    conn = sqlite3.connect('ifpb.db')
+    conn = sqlite3.connect(Nome_DB)
 
     cursor = conn.cursor()
 
@@ -360,7 +539,7 @@ def getDisciplinas():
 def getDisciplina(id):
     logger.info("Listanto disciplinas pelo id.")
     try:
-        conn = sqlite3.connect('ifpb.db')
+        conn = sqlite3.connect(Nome_DB)
 
         cursor = conn.cursor()
 
@@ -389,7 +568,7 @@ def setDisciplinas():
         disciplina = request.get_json()
         nome = disciplina['nome']
 
-        conn = sqlite3.connect("ifpb.db")
+        conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
         cursor.execute("""
             INSERT INTO tb_disciplina(nome)
--- EscolaServicoApp/DDL.py
+++ EscolaServicoApp/DDL.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-conn = sqlite3.connect('ifpb.db')
+conn = sqlite3.connect('EscolaApp_versao2.db')
 
 cursor = conn.cursor()
 cursor.execute("""
     CREATE TABLE tb_escola(
         id_escola INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         nome VARCHAR(45) NOT NULL,
-        logradouro VARCHAR(70) NOT NULL,
-        cidade VARCHAR(45) NOT NULL
+        fk_id_endereco INTEGER,
+        fk_id_campus INTEGER
     );
 """)
 print("Tabela escola criada com sucesso!")
@@ -19,7 +19,9 @@ cursor.execute("""
         nome VARCHAR(45) NOT NULL,
         matricula VARCHAR(12) NOT NULL,
         cpf VARCHAR(11) NOT NULL,
-        nascimento DATE NOT NULL
+        nascimento DATE NOT NULL,
+        fk_id_endereco INTEGER,
+        fk_id_curso INTEGER
     );
 """)
 print("Tabela aluno criada com sucesso!")
@@ -28,7 +30,8 @@ cursor.execute("""
     CREATE TABLE tb_curso(
         id_curso INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         nome VARCHAR(45) NOT NULL,
-        turno VARCHAR(10) NOT NULL
+        turno VARCHAR(10) NOT NULL,
+        fk_id_turno INTEGER
     );
 """)
 print("Tabela curso criada com sucesso!")
@@ -37,7 +40,7 @@ cursor.execute("""
     CREATE TABLE tb_turma(
         id_turma INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         nome VARCHAR(45) NOT NULL,
-        curso VARCHAR(45) NOT NULL
+        fk_id_curso INTEGER
     );
 """)
 print("Tabela turma criada com sucesso!")
@@ -45,10 +48,48 @@ print("Tabela turma criada com sucesso!")
 cursor.execute("""
     CREATE TABLE tb_disciplina(
         id_disciplina INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        nome VARCHAR(45)
+        nome VARCHAR(45),
+        fk_id_professor INTEGER
     );
 """)
 print("Tabela disciplina criada com sucesso!")
 
+cursor.execute("""
+    CREATE TABLE tb_turno(
+        id_turno INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(45)
+    );
+""")
+print("Tabela turno criada com sucesso!")
+
+cursor.execute("""
+    CREATE TABLE tb_campus(
+        id_campus INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        sigla VARCHAR(10),
+        cidade VARCHAR(45)
+    );
+""")
+print("Tabela campus criada com sucesso!")
+
+cursor.execute("""
+    CREATE TABLE tb_professor(
+        id_professor INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(45),
+        fk_id_endereco INTEGER
+    );
+""")
+print("Tabela professor criada com sucesso!")
+
+cursor.execute("""
+    CREATE TABLE tb_endereco(
+        id_endereco INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        logradouro VARCHAR(65),
+        complemento VARCHAR(45),
+        bairro VARCHAR(45),
+        cep VARCHAR(8),
+        numero INTEGER
+    );
+""")
+print("Tabela endereco criada com sucesso!")
 
 conn.close()
