139108b80b6e40e3908cb867b5fee5d3715b2a2e
jhoyceBR <joyce.real01@gmail.com>
2019-08-09 11:55:57 -0300

    correções de bugs

--- EscolaServicoApp/Entidades/Aluno.py
+++ EscolaServicoApp/Entidades/Aluno.py
@@ -1,8 +1,7 @@
+# Classe Aluno
 class Aluno():
     def __init__(self, nome, matricula, cpf, nascimento):
         self.nome = nome
         self.matricula = matricula
         self.cpf = cpf
         self.nascimento = nascimento
-
-        
