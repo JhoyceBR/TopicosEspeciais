2096c8886cbaad72cd77e56902ce8dc8bb7b703b
jhoyceBR <joyce.real01@gmail.com>
2019-08-14 11:54:25 -0300

    atualizações

--- EscolaServicoApp/App.py
+++ EscolaServicoApp/App.py
@@ -103,6 +103,14 @@ disciplina_schema = {
 
 }
 
+professor_schema = {
+    'requered': ['nome', 'fk_id_endereco'],
+    'properties': {
+        'nome': {'type': 'string'},
+        'fk_id_endereco': {'type': 'integer'}
+    }
+}
+
 Nome_DB = "EscolaApp_versao2.db"
 
 @app.route("/alunos", methods=['GET'])
@@ -192,9 +200,57 @@ def setAlunos(): # testado ok
     return (jsonify(aluno))
 
 
-@app.route("/endereco", methods=['GET'])
-def getEnderecos():
-    logger.infor("Listando Endereços.")
+@app.route("/aluno/<int:id>", methods=['PUT'])
+def updateAluno(id):
+    # Receber o JSON.
+    aluno = request.get_json()
+    nome = aluno['nome']
+    matricula = aluno['matricula']
+    cpf = aluno['cpf']
+    nascimento = aluno['nascimento']
+    id_endereco = aluno['fk_id_endereco']
+    id_curso = aluno['fk_id_curso']
+
+    # Buscar o aluno pelo "id".
+    conn = sqlite3.connect(Nome_DB)
+    cursor = conn.cursor()
+
+    # Executar a consulta de pesquisa.​​
+    cursor.execute("""
+        SELECT * FROM tb_aluno WHERE id_aluno = ?;
+    """, (id, ))
+
+    data = cursor.fetchone()
+
+    if data is not None:
+        # Atualizar os dados caso o aluno seja encontrado através do "id".
+        cursor.execute("""
+            UPDATE tb_aluno
+            SET nome=?, matricula=?, cpf=?, nascimento=?, fk_id_endereco=?, fk_id_curso=?
+            WHERE id_aluno = ?;
+        """, (nome, matricula, cpf, nascimento, id_endereco, id_curso, id))
+        conn.commit()
+    else:
+        print("Inserindo")
+        # Inserir novo registro.
+        cursor.execute("""
+            INSERT INTO tb_aluno(nome, matricula, cpf, nascimento, fk_id_endereco, fk_id_curso)
+            VALUES(?, ?, ?, ?, ?, ?);
+        """, (nome, matricula, cpf, nascimento, id_endereco, id_curso))
+        conn.commit()
+        # Identificador do último registro inserido.
+        id = cursor.lastrowid
+        aluno["id_aluno"] = id
+
+    conn.close()
+
+    #Retornar o JSON do aluno atualizado.
+    return jsonify(aluno)
+
+
+@app.route("/enderecos", methods=['GET'])
+def getEnderecos(): # testado ok
+    logger.info("Listanto endereco.")
     try:
         conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
@@ -204,53 +260,50 @@ def getEnderecos():
             FROM tb_endereco;
         """)
 
+        #alterações
         enderecos = []
         for linha in cursor.fetchall():
             endereco = {
-                "id_endereco": linha[0],
-                "logradouro": linha[1],
-                "complemento": linha[2],
-                "bairro": linha[3],
-                "cep": linha[4],
+                "id_endereco":linha[0],
+                "logradouro":linha[1],
+                "complemento":linha[2],
+                "bairro":linha[3],
+                "cep":linha[4],
                 "numero": linha[5]
             }
             enderecos.append(endereco)
-        logger.infor(enderecos)
+        logger.info(enderecos)
         conn.close()
-
-    except(sqlite3.Error):
-        logger.error("Ocorreu um erro.")
-
+    except sqlite3.Error:
+        return("Ocorreu um erro.")
     return jsonify(enderecos)
 
 @app.route("/enderecos/<int:id>", methods=['GET'])
-def getEndereco(id):
-    logger.info("Listando endereços por id: %s"%(id))
+def getEndereco(id): # testado ok
     try:
+        logger.info("Listanto endereco pelo id: %s."%(id))
         conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
 
         cursor.execute("""
             SELECT *
-            FROM tb_endereco
-            WHERE id_endereco = ?;
+            FROM tb_endereco WHERE id_endereco = ?;
         """, (id, ))
 
-        linha = cursor.fechone()
+        linha = cursor.fetchone()
         endereco = {
-            "id_endereco": linha[0],
-            "logradouro": linha[1],
-            "complemento": linha[2],
-            "bairro": linha[3],
-            "cep": linha[4],
+            "id_endereco":linha[0],
+            "logradouro":linha[1],
+            "complemento":linha[2],
+            "bairro":linha[3],
+            "cep":linha[4],
             "numero": linha[5]
         }
 
         conn.close()
     except sqlite3.Error:
         return("Ocorreu um erro.")
-
-    return(jsonify(endereco))
+    return jsonify(endereco)
 
 @app.route("/endereco", methods=['POST'])
 @schema.validate(endereco_schema) # testado ok
@@ -279,6 +332,54 @@ def setEndereco():
         return("Ocorreu um erro.")
 
     return jsonify(endereco)
+
+@app.route("/endereco/<int:id>", methods=['PUT'])
+def updateEndereco(id):
+    # Receber o JSON.
+    endereco = request.get_json()
+    logradouro = endereco['logradouro']
+    complemento = endereco['complemento']
+    bairro = endereco['bairro']
+    cep = endereco['cep']
+    numero = endereco['numero']
+
+
+    # Buscar o aluno pelo "id".
+    conn = sqlite3.connect(Nome_DB)
+    cursor = conn.cursor()
+
+    # Executar a consulta de pesquisa.​​
+    cursor.execute("""
+        SELECT * FROM tb_endereco WHERE id_endereco = ?;
+    """, (id, ))
+
+    data = cursor.fetchone()
+
+    if data is not None:
+        # Atualizar os dados caso o aluno seja encontrado através do "id".
+        cursor.execute("""
+            UPDATE tb_endereco
+            SET logradouro=?, complemento=?, bairro=?, cep=?, numero=?
+            WHERE id_endereco = ?;
+        """, (logradouro, complemento, bairro, cep, numero, id))
+        conn.commit()
+    else:
+        print("Inserindo")
+        # Inserir novo registro.
+        cursor.execute("""
+            INSERT INTO tb_endereco(logradouro, complemento, bairro, cep, numero)
+            VALUES(?, ?, ?, ?, ?);
+        """, (logradouro, complemento, bairro, cep, numero))
+        conn.commit()
+        # Identificador do último registro inserido.
+        id = cursor.lastrowid
+        endereco["id_endereco"] = id
+
+    conn.close()
+
+    #Retornar o JSON do aluno atualizado.
+    return jsonify(endereco)
+
 
 
 @app.route("/cursos", methods=['GET'])
@@ -360,6 +461,52 @@ def setCurso(): #testado ok
 
     return(jsonify(curso))
 
+@app.route("/curso/<int:id>", methods=['PUT'])
+def updateCurso(id):
+    # Receber o JSON.
+    curso = request.get_json()
+    nome = curso['nome']
+    turno = curso['turno']
+    id_turno = curso['fk_id_turno']
+
+    # Buscar o aluno pelo "id".
+    conn = sqlite3.connect(Nome_DB)
+    cursor = conn.cursor()
+
+    # Executar a consulta de pesquisa.​​
+    cursor.execute("""
+        SELECT * FROM tb_curso WHERE id_curso = ?;
+    """, (id, ))
+
+    data = cursor.fetchone()
+
+    if data is not None:
+        # Atualizar os dados caso o aluno seja encontrado através do "id".
+        cursor.execute("""
+            UPDATE tb_curso
+            SET nome=?, turno=?, fk_id_turno=?
+            WHERE id_curso = ?;
+        """, (nome, turno, id_turno, id))
+        conn.commit()
+    else:
+        print("Inserindo")
+        # Inserir novo registro.
+        cursor.execute("""
+            INSERT INTO tb_curso(nome, turno, fk_id_turno)
+            VALUES(?, ?, ?);
+        """, (nome, turno, id_turno))
+        conn.commit()
+        # Identificador do último registro inserido.
+        id = cursor.lastrowid
+        curso["id_curso"] = id
+
+    conn.close()
+
+    #Retornar o JSON do aluno atualizado.
+    return jsonify(curso)
+
+
+
 @app.route("/turnos")
 def getTurnos(): # testado ok
     logger.info("Listando turno.")
@@ -387,29 +534,27 @@ def getTurnos(): # testado ok
     return jsonify(turnos)
 
 @app.route("/turnos/<int:id>", methods=['GET'])
-def getTurno(id):
-    logger.info("Listando turnos por id: %s"%(id))
-
+def getTurno(id): # testado ok
+    logger.info("Listanto turno pelo id: %s"%(id))
     try:
         conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
 
         cursor.execute("""
-            SELECT * FROM tb_turno WHERE id_turno=?;
+            SELECT *
+            FROM tb_turno WHERE id_turno = ?;
         """, (id, ))
 
-        linha = cursor.fechone()
+        linha = cursor.fetchone()
         turno = {
-            "id_turno": linha[0],
-            "nome": linha[1]
+            "id_turno":linha[0],
+            "nome":linha[1]
         }
-
         conn.close()
-
     except(sqlite3.Error):
         logger.error("Ocorreu um erro.")
-
     return jsonify(turno)
+
 
 @app.route("/turno", methods=['POST'])
 @schema.validate(turno_schema) # testado ok
@@ -431,6 +576,49 @@ def setTurno():
         turno["id_turno"] = id
     except(sqlite3.Error):
         logger.error("Ocorreu um erro.")
+    return jsonify(turno)
+
+
+@app.route("/turno/<int:id>", methods=['PUT'])
+def updateTurno(id):
+    # Receber o JSON.
+    turno = request.get_json()
+    nome = turno['nome']
+
+    # Buscar o aluno pelo "id".
+    conn = sqlite3.connect(Nome_DB)
+    cursor = conn.cursor()
+
+    # Executar a consulta de pesquisa.​​
+    cursor.execute("""
+        SELECT * FROM tb_turno WHERE id_turno = ?;
+    """, (id, ))
+
+    data = cursor.fetchone()
+
+    if data is not None:
+        # Atualizar os dados caso o aluno seja encontrado através do "id".
+        cursor.execute("""
+            UPDATE tb_turno
+            SET nome=?
+            WHERE id_turno = ?;
+        """, (nome, id))
+        conn.commit()
+    else:
+        print("Inserindo")
+        # Inserir novo registro.
+        cursor.execute("""
+            INSERT INTO tb_turno(nome)
+            VALUES(?);
+        """, (nome))
+        conn.commit()
+        # Identificador do último registro inserido.
+        id = cursor.lastrowid
+        turno["id_turno"] = id
+
+    conn.close()
+
+    #Retornar o JSON do aluno atualizado.
     return jsonify(turno)
 
 
@@ -466,8 +654,8 @@ def getEscolas(): # testado ok
 
 
 @app.route("/escolas/<int:id>", methods=['GET'])
-def getEscola(id):
-    logger.info("Listanto escola pelo id: %s" %(id))
+def getEscola(id): # testado ok
+    logger.info("Listanto escola pelo id: %s"%(id))
     try:
         conn = sqlite3.connect(Nome_DB)
         cursor = conn.cursor()
@@ -476,44 +664,85 @@ def getEscola(id):
             SELECT *
             FROM tb_escola WHERE id_escola = ?;
         """, (id, ))
-        linha = cursor.fechone()
+
+        linha = cursor.fetchone()
         escola = {
             "id_escola":linha[0],
             "nome":linha[1],
-            "id_fk_endereco":linha[2],
-            "fk_id_campus":linha[3]
+            "id_endereco":linha[2],
+            "id_campus":linha[3]
         }
-
         conn.close()
-    except sqlite3.Error:
-        return("Ocorreu um erro.")
-
+    except(sqlite3.Error):
+        logger.error("Ocorreu um erro.")
     return jsonify(escola)
+
 
 @app.route("/escola", methods=['POST'])
 @schema.validate(escola_schema)
-def setEscola():
+def setEscolas(): # testado ok
     logger.info("Cadastrando escola.")
-    try:
-        escola = request.get_json()
-        nome = escola['nome']
-        id_endereco = escola['fk_id_endereco']
-        id_campus = escola['fk_id_campus']
+    escola = request.get_json()
+    nome = escola['nome']
+    id_endereco = escola['fk_id_endereco']
+    id_campus = escola['fk_id_campus']
 
-        conn = sqlite3.connect(Nome_DB)
-        cursor = conn.cursor()
+    conn = sqlite3.connect(Nome_DB)
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO tb_escola(nome, fk_id_endereco, fk_id_campus)
+        VALUES(?, ?, ?);
+    """, (nome, id_endereco, id_campus))
+    conn.commit()
+    conn.close()
+
+    id = cursor.lastrowid
+    escola['id_escola'] = id
+
+    return (jsonify(escola))
+
+
+@app.route("/escola/<int:id>", methods=['PUT'])
+def updateEscola(id):
+    # Receber o JSON.
+    escola = request.get_json()
+    nome = escola['nome']
+    id_endereco = escola['fk_id_endereco']
+    id_campus = escola['fk_id_campus']
+
+    conn = sqlite3.connect(Nome_DB)
+    cursor = conn.cursor()
+
+    # Executar a consulta de pesquisa.​​
+    cursor.execute("""
+        SELECT * FROM tb_escola WHERE id_escola = ?;
+    """, (id, ))
+
+    data = cursor.fetchone()
+
+    if data is not None:
+
         cursor.execute("""
-            INSERT INTO tb_escola(nome, id_fk_endereco, id_fk_campus)
+            UPDATE tb_escola
+            SET nome=?, fk_id_endereco=?, fk_id_campus=?
+            WHERE id_escola = ?;
+        """, (nome, id_endereco, id_campus, id))
+        conn.commit()
+    else:
+        print("Inserindo")
+        # Inserir novo registro.
+        cursor.execute("""
+            INSERT INTO tb_escola(nome, fk_id_endereco, fk_id_campus)
             VALUES(?, ?, ?);
         """, (nome, id_endereco, id_campus))
         conn.commit()
-        conn.close()
-
+        # Identificador do último registro inserido.
         id = cursor.lastrowid
-        escola['id_escola'] = id
-    except sqlite3.Error:
-        return("Ocorreu um erro.")
+        escola["id_escola"] = id
 
+    conn.close()
+
+    #Retornar o JSON do aluno atualizado.
     return jsonify(escola)
 
 
@@ -543,6 +772,8 @@ def getCampi(): # testado ok
     except(sqlite3.Error):
         logger.error("Ocorreu um erro.")
     return jsonify(campi)
+
+#####continuar updates
 
 @app.route("/campi/<int:id>", methods=['GET'])
 def getCampus(id): # testado ok
@@ -694,11 +925,10 @@ def getDisciplinas(): # testado ok
 
 
 @app.route("/disciplinas/<int:id>", methods=['GET'])
-def getDisciplina(id):
-    logger.info("Listanto disciplinas pelo id.")
+def getDisciplina(id): # testado ok
     try:
+        logger.info("Listanto disciplina pelo id: %s."%(id))
         conn = sqlite3.connect(Nome_DB)
-
         cursor = conn.cursor()
 
         cursor.execute("""
@@ -706,17 +936,16 @@ def getDisciplina(id):
             FROM tb_disciplina WHERE id_disciplina = ?;
         """, (id, ))
 
-        linha = cursor.fechone()
+        linha = cursor.fetchone()
         disciplina = {
             "id_disciplina":linha[0],
             "nome":linha[1],
-            "id_professor": linha[2]
+            "fk_id_professor": linha[2]
         }
 
         conn.close()
     except sqlite3.Error:
         return("Ocorreu um erro.")
-
     return jsonify(disciplina)
 
 @app.route("/disciplina", methods=['POST'])
@@ -742,7 +971,80 @@ def setDisciplina():
         logger.error("Ocorreu um erro.")
     return jsonify(disciplina)
 
-#criar professor! 
+@app.route("/professores", methods=['GET'])
+def getProfessores(): # testado ok
+    logger.info("Listanto professores.")
+    try:
+        conn = sqlite3.connect(Nome_DB)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT *
+            FROM tb_professor;
+        """)
+
+        #alterações
+        professores = []
+        for linha in cursor.fetchall():
+            professor = {
+                "id_professor":linha[0],
+                "nome":linha[1],
+                "fk_id_endereco": linha[2]
+            }
+            professores.append(professor)
+        logger.info(professores)
+        conn.close()
+    except sqlite3.Error:
+        return("Ocorreu um erro.")
+    return jsonify(professores)
+
+
+@app.route("/professores/<int:id>", methods=['GET'])
+def getProfessor(id): # testado ok
+    try:
+        logger.info("Listanto professor pelo id: %s."%(id))
+        conn = sqlite3.connect(Nome_DB)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT *
+            FROM tb_professor WHERE id_professor = ?;
+        """, (id, ))
+
+        linha = cursor.fetchone()
+        professor = {
+            "id_professor":linha[0],
+            "nome":linha[1],
+            "fk_id_endereco": linha[2]
+        }
+
+        conn.close()
+    except sqlite3.Error:
+        return("Ocorreu um erro.")
+    return jsonify(professor)
+
+@app.route("/professor", methods=['POST'])
+@schema.validate(professor_schema)
+def setProfessor(): # testado ok
+    logger.info("Cadastrando professor.")
+    professor = request.get_json()
+    nome = professor['nome']
+    id_endereco = professor['fk_id_endereco']
+
+    conn = sqlite3.connect(Nome_DB)
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO tb_professor(nome, fk_id_endereco)
+        VALUES(?, ?);
+    """, (nome,id_endereco))
+    conn.commit()
+    conn.close()
+
+    id = cursor.lastrowid
+    professor['id_professor'] = id
+
+    return (jsonify(professor))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, use_reloader=True)
--- EscolaServicoApp/Entidades/Aluno.py
+++ EscolaServicoApp/Entidades/Aluno.py
@@ -1,7 +1,9 @@
 # Classe Aluno
 class Aluno():
-    def __init__(self, nome, matricula, cpf, nascimento):
+    def __init__(self, nome, matricula, cpf, nascimento, id_endereco, id_curso):
         self.nome = nome
         self.matricula = matricula
         self.cpf = cpf
         self.nascimento = nascimento
+        self.id_endereco = id_endereco
+        self.id_curso = id_curso
